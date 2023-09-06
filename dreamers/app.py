4039768b0d555560918a7eb4ab907b8cd19c3c11
Dan Langford <721364+danlangford@users.noreply.github.com>
2023-09-05 20:35:10 -0600

    try to set up pipeline on app startup & more prompts and more images

--- dreamers/app.py
+++ dreamers/app.py
@@ -4,6 +4,8 @@ import torch
 import openai
 app = Flask(__name__)
 
+global base
+
 @app.route('/')
 def index():
     return render_template('form.html')
@@ -19,38 +21,46 @@ def generate_avatar():
     print(name, description)
     response = openai.Completion.create(
       engine="text-davinci-002",
-      prompt=f"acting as a caricature artist describe what you would see as the looks of a sterotypical person named {name} who also embodies the ideas of \"{description}\". the description should be only a few sentences and not include the words \"caricature\", \"stereotypical\",or \"stereotype\" ",
+      prompt=f'acting as a caricature artist describe what you would see as the looks of a sterotypical person named "{name}" who also embodies the ideas of "{description}". the description should be only a few sentences and not include the words "caricature", "stereotypical",or "stereotype"',
       max_tokens=50
     )
     gpt3_prompt = response.choices[0].text.strip()
-    # Prepare your pipeline
-    base = StableDiffusionXLPipeline.from_single_file(
-        './sd_xl_base_1.0_0.9vae.safetensors', 
-        torch_dtype=torch.float16,
-        variant="fp16",
-        use_safetensors=True
-    )
-    base.to('cuda')
-    
-    # Use the user-provided description as the prompt
-    # base_prompt = 'avatar ' + description
-    # second_prompt = 'avatar ' + description + ', animated, happy, young, anime'
-    # third_prompt = 'avatar ' + description + ', ugly, funny, farty, ascii art'
     
     # prompts = [base_prompt, second_prompt, third_prompt]
     neg_prompt = "low res, ugly, bad hands, too many digits, bad teeth, blurry, blurred background"
     image = base(prompt=gpt3_prompt, negative_prompt=neg_prompt).images[0]
     image.save('./static/avatar.png')
 
-    # for i, prompt in enumerate(prompts):
-        # image = base(prompt=prompt, negative_prompt=neg_prompt).images[0]
-        # Assuming the image object has a save method, otherwise convert it to PIL image and save
-        # image.save(f'{i}_img.png')
+    # Use the user-provided description as the prompt
+    prompts = [
+        f"avatar {description}",
+        f"avatar {description}, animated, happy, young, anime",
+        f"avatar {description}, ugly, funny, farty, ascii art",
+        f'profile avatar in the style of "avatar: the last Airbender". the image should be for a user named "{name}" and should embody the essence of "{description}"',
+        f'3d, style of funko pop toy, caricature, and should embody the essence of "{description}"'
+                ]
+
+    for i, prompt in prompts:
+        image = base(prompt=prompt, negative_prompt=neg_prompt).images[0]
+        #Assuming the image object has a save method, otherwise convert it to PIL image and save
+        image.save(f"./static/avatar{i}.png")
+
     return redirect(url_for("images"))
     # return f'Avatars generated! <a href="/static/avatar.png">View first image</a>'
     # return f'Avatar generated for {name} with the description: {description}'
+
 @app.route('/static/<path:filename>')
 def serve_static(filename):
     return send_from_directory('static', filename)
+
 if __name__ == '__main__':
+    # Prepare your pipeline
+    base = StableDiffusionXLPipeline.from_single_file(
+        './sd_xl_base_1.0_0.9vae.safetensors',
+        torch_dtype=torch.float16,
+        variant="fp16",
+        use_safetensors=True
+    )
+    base.to('cuda')
+
     app.run()
